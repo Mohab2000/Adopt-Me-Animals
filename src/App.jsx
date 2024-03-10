11ca570b4f06d4d0723c05065c60dcfe72137ad0
Mohab Ahmed Mohamed | Ejada Systems Ltd. <mohab@ejadacairo.local>
2024-03-10 11:02:19 +0200

    NotFound Component and Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SearchParams from './pages/SearchParams';
+import Details from './pages/Details';
+import NotFound from './pages/NotFound';
+
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 const container = document.getElementById('root');
