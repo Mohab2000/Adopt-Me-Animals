70de4d9f7a418f0d463d13a71b6353cb7fb800d0
Mohab Ahmed Mohamed | Ejada Systems Ltd. <mohabahmed20005@gmail.com>
2024-03-10 11:55:39 +0200

    Routing & Navigating

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import SearchParams from './pages/SearchParams';
 import Details from './pages/Details';
 import NotFound from './pages/NotFound';
@@ -7,7 +7,9 @@ import NotFound from './pages/NotFound';
 const App = () => {
   return (
     <BrowserRouter>
-      <h1>Adopt Me!</h1>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
       <Routes>
         <Route path="/details/:id" element={<Details />} />
         <Route path="/" element={<SearchParams />} />
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,4 +1,18 @@
+import { useParams, useNavigate } from 'react-router-dom';
 const Details = () => {
-  return <h1>Pet Details</h1>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  return (
+    <div className="details">
+      <h3>Pet {id} Details Page</h3>
+      <button
+        onClick={() => {
+          navigate('/');
+        }}
+      >
+        Back
+      </button>
+    </div>
+  );
 };
 export default Details;
